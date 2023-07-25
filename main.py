06813993e5907cf4a8dd681155678dbc9a856914
Gontarenok <vika030395@gmail.com>
2023-07-25 09:17:39 +0300

    Completed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import json
 import pandas as pd
 
-# data = pd.read_json('trial_task.json')
-# print(data)
-
 with open('trial_task.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
@@ -21,79 +18,121 @@ summary_data = extracted_data.merge(products_data, how='outer')
 # TASK 1
 
 # Группируем по id заказа и номеру склада и получаем сумму товаров в каждом заказе
-group_data = summary_data.groupby(['order_id', 'warehouse_name', 'highway_cost'])['quantity'].sum()
-
-# Превращаем объект выборки обратно в DataFrame
-group_data = group_data.reset_index()
+group_data = summary_data.groupby(['order_id', 'warehouse_name', 'highway_cost'])['quantity'].sum().reset_index()
 
 # Добавляем новый столбец со стоимостью доставки в каждом заказе
 group_data['cost_delivery'] = (abs(group_data['highway_cost']) / group_data['quantity'])
 
 # Группируем для вывода стоимости доставки по каждому складу
-cost_delivery = group_data.groupby('warehouse_name')['cost_delivery'].mean()
-cost_delivery = cost_delivery.reset_index()
+cost_delivery = group_data.groupby('warehouse_name')['cost_delivery'].mean().reset_index()
 cost_delivery_renamed = cost_delivery.rename(
     columns={
         'warehouse_name': 'Склад',
         'cost_delivery': 'Стоимость доставки'
     }
 )
+
+print('Результат выполнения 1 задания: ')
 print(cost_delivery_renamed)
+
+# cost_delivery_renamed.to_excel('task_1.xlsx', sheet_name='data', index=True)
 
 # TASK 2
 
+# Делаем автономную копию DataFrame
 summary_data_2 = summary_data.copy(deep=True)
 
 full_table = pd.merge(summary_data_2, cost_delivery, how='inner')
-# print(full_table)
-group_products = full_table.groupby(['product', 'price', 'cost_delivery'])['quantity'].sum().reset_index()
 
-
+# Добавляем столбцы:
 # Доход
-group_products['income'] = group_products['price'] * group_products['quantity']
+full_table['income'] = full_table['price'] * full_table['quantity']
 # Расход
-group_products['expenses'] = group_products['cost_delivery'] * group_products['quantity']
+full_table['expenses'] = full_table['cost_delivery'] * full_table['quantity']
 # Прибыль
-group_products['profit'] = group_products['income'] - group_products['expenses']
+full_table['profit'] = full_table['income'] - full_table['expenses']
 
-group_products = group_products.groupby(['product'])[['quantity', 'income', 'expenses', 'profit']].sum().reset_index()
+group_products = full_table.groupby(['product'])[['quantity', 'income', 'expenses', 'profit']].sum().reset_index()
 
+print('Результат выполнения 2 задания: ')
 print(group_products)
 
-# group_products = full_table.groupby(['product'])[('quantity', 'income', 'expenses', 'profit')].sum()
-# print(group_products)
+# group_products.to_excel('task_2.xlsx', sheet_name='data', index=True)
 
-# result_table = pd.DataFrame()
-# result_table['product'] = full_table['product']
-# # суммарное количество
-# result_table['quantity'] = full_table['quantity']
-# # суммарный доход
-# result_table['income'] = full_table['quantity']
-# # суммарный расход
-# result_table['expenses'] = full_table['quantity']
-# # суммарная прибыль
-# result_table['profit'] = full_table['quantity']
-# print(result_table)
+# TASK 3
+
+full_table_2 = full_table.copy(deep=True)
+full_table_2 = full_table_2.sort_values(by='order_id')
+
+group_orders = full_table_2.groupby(['order_id'])['profit'].sum().reset_index()
+group_orders_renamed = group_orders.rename(
+    columns={'profit': 'order_profit'}
+)
+print('Результат выполнения  3 задания: ')
+print('Прибыль, полученная с заказов:')
+print(group_orders_renamed)
+
+# group_orders_renamed.to_excel('task_3.xlsx', sheet_name='data', index=True)
+
+print('Средняя прибыль заказов:')
+print(group_orders['profit'].mean())
+
+# TASK 4
+
+full_table_3 = full_table.copy(deep=True)
+# Группируем по складам и проданным в них продуктам
+group_warehouses = full_table_3.groupby(['warehouse_name', 'product'])[['quantity', 'profit']].sum().reset_index()
+# Высчитываем прибыль по складам
+warehouses_profit = group_warehouses.groupby(['warehouse_name'])['profit'].sum().reset_index()
+# Переименовываем перед слиянием таблиц, т.к. в исходной уже есть столбец profit
+warehouses_profit = warehouses_profit.rename(
+    columns={'profit': 'warehouses_profit'}
+)
+# В сгруппированную таблицу добавляем прибыль склада для расчета процента товара от него
+group_warehouses = pd.merge(group_warehouses, warehouses_profit, how='inner')
+# Высчитываем процент прибыли продукта заказанного из определенного склада к прибыли этого склада
+group_warehouses['percent_profit_product_of_warehouse'] = (group_warehouses['profit'] * 100) / group_warehouses[
+    'warehouses_profit']
+
+print('Результат выполнения 4 задания: ')
+print(group_warehouses)
+
+# group_warehouses.to_excel('task_4.xlsx', sheet_name='data', index=True)
+
+# TASK 5
+
+group_warehouses_sort = group_warehouses.copy(deep=True)
+# сортировка по убыванию
+group_warehouses_sort = group_warehouses_sort.sort_values(['warehouse_name', 'percent_profit_product_of_warehouse'],
+                                                          ascending=[True, False])
+# добавление столбца накопленного процента
+group_warehouses_sort['accumulated_percent_profit_product_of_warehouse'] = \
+    group_warehouses_sort.groupby('warehouse_name')['percent_profit_product_of_warehouse'].cumsum()
+
+print('Результат выполнения 5 задания: ')
+print(group_warehouses_sort)
+
+# group_warehouses_sort.to_excel('task_5.xlsx', sheet_name='data', index=True)
 
 
+# TASK 6
+
+# Создаем функцию для определения категорий
+def category(row):
+    if row['accumulated_percent_profit_product_of_warehouse'] <= 70:
+        return 'A'
+    elif 70 < row['accumulated_percent_profit_product_of_warehouse'] <= 90:
+        return 'B'
+    else:
+        return 'C'
 
 
-# summary_data.to_excel('summary_data_2.xlsx', sheet_name='data', index=True)
+category_table = group_warehouses_sort.copy(deep=True)
 
-# print(data.tail(10))
-# print(data.dtypes)
-# print(data[['order_id', 'products']])
+# Применяем функцию для столбца с помощью метода apply()
+category_table['category'] = category_table.apply(category, axis=1)
 
-# data.to_excel('data.xlsx', sheet_name='data', index=False)
+print('Результат выполнения 6 задания: ')
+print(category_table)
 
-# data_mordor = data[data['warehouse_name'] == 'Мордор']
-# print(data_mordor)
-
-# data_mordor = data.loc[data['warehouse_name'] == 'Мордор', 'highway_cost']
-# print(data_mordor)
-
-# data['highway_cost'].plot()
-# plt.show()
-
-
-# print(data['products'].describe())
+# category_table.to_excel('task_6.xlsx', sheet_name='data', index=True)
